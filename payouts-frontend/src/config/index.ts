741a35fecbc540a069900584491776ec1e9a7e84
OleanjiKingCode <holamilean56@gmail.com>
2022-10-06 21:29:24 +0100

    frontend of editor payouts

---
+++ payouts-frontend/src/config/index.ts
@@ -0,0 +1,8 @@
+const config = {
+  alchemyApiKey: String(process.env.NEXT_PUBLIC_ALCHEMY_API_KEY),
+  alchemyChain: "mumbai",
+  chainId: "80001",
+  chainName: "mumbai",
+};
+
+export default config;
--- payouts-frontend/src/config/wagmi.ts
+++ payouts-frontend/src/config/wagmi.ts
@@ -2,13 +2,15 @@ import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { chain, configureChains } from 'wagmi'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
-import config from '../../../../../../../../../Documents/IQ/iq-ui/src/config/index'
+import config from './index'
 
 type Connector = MetaMaskConnector | WalletConnectConnector
 const chainArray =
   config.alchemyChain === 'goerli' ? [chain.goerli] : [chain.mainnet]
 export const { chains, provider } = configureChains(chainArray, [
   alchemyProvider({ alchemyId: config.alchemyApiKey, weight: 1 }),
+  // infuraProvider({ infuraId: config.infuraId, weight: 2 }),
+  // publicProvider({ weight: 3 }),
 ])
 
 export const connectors: Connector[] = [
